364a231e20f9921d2899f01621caf1ac66348e2e
alexis <arrghr2006@gmail.com>
2023-06-28 22:33:11 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import './globals.css';
+
+export const metadata = {
+  title: 'flexible',
+  description: 'Showcase and discover remarable developer projects.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main>
+          {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  )
+}
